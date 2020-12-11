45dbb49f80fab81b42fd5ceeedb78b3546df4e81
gefest <gefest@me.com>
2020-12-11 20:36:54 +0300

    les 2 hw

---
+++ homeworks/les_2/task_2.py
@@ -0,0 +1,18 @@
+# homework lesson: 2, task:2
+
+"""
+Для списка реализовать обмен значений соседних элементов, т.е.
+Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+При нечетном количестве элементов последний сохранить на своем месте.
+Для заполнения списка элементов необходимо использовать функцию input().
+"""
+
+user_answer = input('введите список через запятую')
+user_list = user_answer.split(',')
+
+idx = 0
+len_list = len(user_list) - 1
+while idx < len_list:
+    user_list[idx], user_list[idx + 1] = user_list[idx + 1], user_list[idx]
+    idx += 2
+print(user_list)
---
+++ les2_1.py
@@ -0,0 +1,40 @@
+"""
+неизменяемые типы
+int
+float
+bool
+
+
+None
+
+str
+tuple
+frozenset
+Изменяемые
+list
+
+dict
+set
+
+"""
+
+some = 'hjhshwhriwgihqfhkcbxfgjsgfhkfhkfghkfh'
+
+# for item in some:
+#     print(item)
+
+# idx = 0
+# while idx < len(some):
+#     item = some[idx:idx+2]
+#     print(item)
+#     idx += 2
+
+
+some_list = [1, 2, 3, 'some string', True, 22.3]
+
+some_dict = {
+    'one': 1652,
+    'two': 357336,
+    'user_name': 'SOme name',
+    'user_age': 22,
+}
